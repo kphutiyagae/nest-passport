c950e130e0a420d6c94166c8cc52c0bb0cb6b433
kphutiyagae <kopanophutiyagae@gmail.com>
2023-10-04 13:34:13 +0200

    feat: :sparkles: Added user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+  id: number;
+  name: string;
+};
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly usersList: User[] = [
+    { id: 0, name: 'John' },
+    { id: 1, name: 'Jane' },
+    { id: 2, name: 'Tom' },
+  ];
+
+  async findOne(userId: number) {
+    return this.usersList.find((user) => user.id === userId);
+  }
+}
